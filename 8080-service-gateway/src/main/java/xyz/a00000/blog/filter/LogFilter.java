a772f1e024cda7c80e2af44a90840e0f8589bb97
A0000000000 <mengxin_zzl@outlook.com>
2021-02-22 19:48:40 +0800

    修改部分逻辑.

--- 8080-service-gateway/src/main/java/xyz/a00000/blog/filter/LogFilter.java
+++ 8080-service-gateway/src/main/java/xyz/a00000/blog/filter/LogFilter.java
@@ -1,5 +1,6 @@
 package xyz.a00000.blog.filter;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
@@ -12,11 +13,8 @@ import reactor.core.publisher.Mono;
 import xyz.a00000.blog.bean.orm.AccessInfo;
 import xyz.a00000.blog.mapper.AccessInfoMapper;
 
-import java.net.InetSocketAddress;
 import java.net.URI;
-import java.util.Arrays;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 
 @Component
 @Slf4j
@@ -27,23 +25,32 @@ public class LogFilter implements GlobalFilter, Ordered {
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-        log.info("获取网关访问的地址和ip");
-        ServerHttpRequest request = exchange.getRequest();
-        URI uri = request.getURI();
-        log.info("获得请求头中的ip.");
-        List<String> ip = request.getHeaders().getOrEmpty("ip");
-        log.info("构建访问信息对象.");
-        AccessInfo info = new AccessInfo();
-        info.setUrl(uri.getPath());
-        if (ip.size() == 0) {
-            info.setIp(String.format("{\"local\": \"%s\", \"remote\": \"%s\"}", request.getLocalAddress().getHostName(), request.getRemoteAddress().getHostName()));
-        } else {
-            info.setIp(Arrays.toString(ip.toArray()));
+        try {
+            log.info("获取网关访问的地址和ip");
+            ServerHttpRequest request = exchange.getRequest();
+            URI uri = request.getURI();
+            log.info("获得请求头中的ip.");
+            List<String> ip = request.getHeaders().getOrEmpty("ip");
+            log.info("构建访问信息对象.");
+            AccessInfo info = new AccessInfo();
+            info.setUrl(uri.getPath());
+            if (ip.size() == 0) {
+                Map<String, String> ipInfo = new HashMap<>();
+                ipInfo.put("local", Objects.requireNonNull(request.getLocalAddress()).getHostName());
+                ipInfo.put("remote", Objects.requireNonNull(request.getRemoteAddress()).getHostName());
+                ObjectMapper mapper = new ObjectMapper();
+                String data = mapper.writeValueAsString(ipInfo);
+                info.setIp(data);
+            } else {
+                info.setIp(Arrays.toString(ip.toArray()));
+            }
+            info.setAccessTime(new Date());
+            log.info("持久化数据.");
+            accessInfoMapper.insert(info);
+            log.info("记录完成, 准备放行.");
+        } catch (Exception ignore) {
+            log.info("过滤器发生异常, 信息: " + ignore.getMessage());
         }
-        info.setAccessTime(new Date());
-        log.info("持久化数据.");
-        accessInfoMapper.insert(info);
-        log.info("记录完成, 准备放行.");
         Mono<Void> filter = chain.filter(exchange);
         log.info("拦截完成.");
         return filter;
